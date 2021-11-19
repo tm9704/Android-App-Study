75d4de9968261dd3bf6e73ce673d374d7d75b8a9
tm9704 <tm9704@korea.ac.kr>
2021-11-19 14:01:32 +0900

    TextView Change Text

--- Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
@@ -2,10 +2,21 @@ package com.chobo.practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //변수 생성
+        val tv: TextView = findViewById(R.id.tv_hello)
+        //내용 선택
+        tv.text = "안녕"
+
+//        val textView: TextView = findViewById(R.id.android_text) as TextView
+//        textView.setOnClickListener {
+//            textView.text = getString(R.string.name)
+//        }
     }
 }
