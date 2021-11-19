4858ded7a993e10751e9564ed18b28238a6fc9eb
tm9704 <tm9704@korea.ac.kr>
2021-11-19 14:05:52 +0900

    TextView Change Text With Button

--- Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.chobo.practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -9,11 +10,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //변수 생성
+        //변수 생성 (TextView, Button은 객체)
         val tv: TextView = findViewById(R.id.tv_hello)
-        //내용 선택
-        tv.text = "안녕"
+        val btn: Button = findViewById(R.id.btn_kor)
 
+        //버튼에 기능 추가
+        btn.setOnClickListener {
+            tv.text = "안녕"
+        }
+        
 //        val textView: TextView = findViewById(R.id.android_text) as TextView
 //        textView.setOnClickListener {
 //            textView.text = getString(R.string.name)
