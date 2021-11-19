ad942813319a7c64c83bcf1e7cbedddffd5fcdf5
tm9704 <tm9704@korea.ac.kr>
2021-11-19 14:44:07 +0900

    Timer With Button

--- Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
@@ -12,24 +12,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var sec : Int = 0
+        //변수 생성 (TextView, Button은 객체)
         val tv: TextView = findViewById(R.id.tv_hello)
+        val btn: Button = findViewById(R.id.btn_kor)
 
-        //period 는 주기 1000이면 1/1000초 마다 함수가 돌아
-        timer(period = 1000){
-            sec++
-            // 실시간으로 변화
-            runOnUiThread {
-                tv.text = sec.toString()
+        //버튼에 기능 추가
+        btn.setOnClickListener {
+            //period 는 주기 1000이면 1/1000초 마다 함수가 돎
+            timer(period = 1000){
+                sec++
+                // 실시간으로 변화
+                runOnUiThread {
+                    tv.text = sec.toString()
+                }
             }
         }
-
-//        //변수 생성 (TextView, Button은 객체)
-//        val tv: TextView = findViewById(R.id.tv_hello)
-//        val btn: Button = findViewById(R.id.btn_kor)
-//
-//        //버튼에 기능 추가
-//        btn.setOnClickListener {
-//            tv.text = "안녕"
-//        }
     }
 }
