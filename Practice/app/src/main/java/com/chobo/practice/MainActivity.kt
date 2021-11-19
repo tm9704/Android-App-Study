d355e4c6104e3d9a71d1880efd83f4ba72d63091
tm9704 <tm9704@korea.ac.kr>
2021-11-19 14:24:52 +0900

    Timer Failed

--- Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
@@ -4,24 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //변수 생성 (TextView, Button은 객체)
+        var sec : Int = 0
         val tv: TextView = findViewById(R.id.tv_hello)
-        val btn: Button = findViewById(R.id.btn_kor)
 
-        //버튼에 기능 추가
-        btn.setOnClickListener {
-            tv.text = "안녕"
+        //period 는 주기 1000이면 1/1000초 마다 함수가 돌아
+        timer(period = 1000){
+            sec++
+            tv.text = sec.toString()
         }
-        
-//        val textView: TextView = findViewById(R.id.android_text) as TextView
-//        textView.setOnClickListener {
-//            textView.text = getString(R.string.name)
+
+//        //변수 생성 (TextView, Button은 객체)
+//        val tv: TextView = findViewById(R.id.tv_hello)
+//        val btn: Button = findViewById(R.id.btn_kor)
+//
+//        //버튼에 기능 추가
+//        btn.setOnClickListener {
+//            tv.text = "안녕"
 //        }
     }
 }
