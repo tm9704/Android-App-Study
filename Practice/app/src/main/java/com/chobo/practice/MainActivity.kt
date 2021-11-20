7a2d7cfac60260a7623958805e19806128b9a111
tm9704 <tm9704@korea.ac.kr>
2021-11-20 17:17:21 +0900

    Add Start Page

--- Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
+++ Practice/app/src/main/java/com/chobo/practice/MainActivity.kt
@@ -75,6 +75,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         main()
